20f7665f04d7a20809e3ae47a4c5177c21b9d30c
Nicholas Stienz <nickstienz@gmail.com>
2022-11-01 21:29:30 -0500

    Added the file input plus cleanup

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,8 @@ mod token;
 use lexer::Lexer;
 
 fn main() {
-    println!("=== Program 1 ===");
-    let program1 = "ATTACKMODE HID\nDELAY 500\nSTRING \"Hello World\"";
-    println!("{}", program1);
-
-    println!("=== Tokens 1 ===");
-    let tokens1 = Lexer::new(program1).lex();
-    tokens1.iter().for_each(|token| {
-        println!("{:?}", token);
-    });
+    Lexer::new(&std::fs::read_to_string("input.txt").unwrap())
+        .lex()
+        .iter()
+        .for_each(|t| println!("{:?}", t));
 }
